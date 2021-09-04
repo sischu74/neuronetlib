4b35fd9294230668ccdf3c9f4686b3f32fc7e5d0
Sascha Kehrli <sascha.kehrli@gmail.com>
2021-09-04 13:22:04 +0200

    Create main.py
    Added feedforward network constructor I already built in a Jupyter Notebook.

---
+++ main.py
@@ -0,0 +1,232 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+x = np.array([[0], [0.1], [0.2], [0.3], [0.4], [0.5], [0.6], [0.7], [0.8], [0.9], [1]])
+y = np.array([[0], [0.1], [0.2], [0.3], [0.7], [1], [0.7], [0.3], [0.2], [0.1], [0]])
+
+class layer():
+    def __init__(self, layer_nr, nodes):
+        self.layer_nr = layer_nr # position of the layer
+        self.nodes = nodes # list of the nodes belonging to this layer
+
+    # method returns a list of the node values in the layer
+    def value_list(self):
+        values = []
+        for neuron in self.nodes:
+            values.append(neuron.value)
+        return values
+
+class node():
+    def __init__(self, w, b=0, child_nodes=[], parent_nodes=[], value=0, derivative=0):
+        self.w = w # list of this node's weight parameters
+        self.b = b # this node's offset parameter
+        self.child_nodes = child_nodes # list of this node's child nodes
+        self.parent_nodes = parent_nodes # list of this node's parent nodes
+        self.value = value # calculated value of this node. used for prediction and backpropagation
+        self.derivative = derivative # derivative of loss function with respect to this node. calculated in 'train' procedure
+
+class ffnn():
+    def __init__(self, h_layers, task = 'regression', activ_func = 'relu', learning_rate = 0.1, batch_size = 0.1):
+        self.h_layers = h_layers
+        self.task = task
+        self.activ_func = activ_func
+        self.learning_rate = learning_rate
+        self.batch_size = batch_size
+        self.layers = 0 # layers are later added by the 'construct' method
+
+    def predict(self, x, y):
+        layers = self.layers
+        error = 0
+        preds = []
+
+        for sample in range(x.shape[0]):
+            # fill in the values of the input layer with the feature values
+            for i in range(x.shape[1]):
+                layers[0].nodes[i].value = x[sample][i]
+
+            # calculate all node values as the dot product of its w vector and the parent nodes' values plus an offset 'b'
+            for i in range(1,len(layers)): # cycle through layers
+                for neuron in layers[i].nodes: # cycle through nodes in layer
+                    neuron.value = np.dot(neuron.w, layers[i-1].value_list()) + neuron.b
+                    if self.activ_func == 'relu':
+                        neuron.value = max(0, neuron.value)
+                    elif self.activ_func == 'softplus':
+                        neuron.value = np.log(1+np.exp(neuron.value))
+            # apply softmax function to last layer for classification
+            if self.task == 'classification':
+                for neuron in range(len(layers[-1].nodes)): # cycle through output nodes
+                    denominator = 0
+                    for others in range(len(layers[-1].nodes)): # cycle through other nodes in the layer
+                        denominator += np.exp(layers[-1].nodes[others].value)
+                    neuron.value = np.exp(neuron.value)/denominator
+
+            # error calculation for regression
+            if self.task == 'regression':
+                preds.append(layers[-1].nodes[0].value)
+                error += (y[sample] - float(layers[-1].nodes[0].value))**2
+
+            # error calculation for classification
+            elif self.task == 'classification':
+                # predict the maximum value in the output layer
+                output_values = layers[-1].value_list
+                preds.append(output_values.index(max(output_values)))
+                #...add error calculation
+                #...
+                #...
+
+        return error, preds
+
+    def predictions(self, x, y):
+        # function is only for user access and prints the figures nicely.
+        # basically a pretty-print of the 'internal' predict function above
+        errors, preds = self.predict(x,y)
+        print('Test Error (per sample):', errors[0]/x.shape[0])
+        print('Predicted values:', preds)
+
+    def construct(self, x):
+        # construct the layers and nodes specified
+        global node # tell python that we mean the global class "node" and not a local variable
+        layers = [] # create an empty list that we fill with layers containing the nodes
+
+        # create an input layer with d nodes. these don't contain any weights
+        layers.append(layer(0,[]))
+        for i in range(x.shape[1]): # one input node for every feature the input data contains
+            layers[0].nodes.append(node([]))
+
+        # create the hidden layers
+        for i in range(1,len(self.h_layers)+1):
+            layers.append(layer(i,[])) # create a layer object
+            for j in range(self.h_layers[i-1]): # create one hidden node per specified
+                layers[i].nodes.append(node([]))
+
+        # create an output layer
+        if self.task == 'classification': # output node contains as many parameters as the last hidden layer contains nodes
+            layers.append(layer(len(layers),[]))
+            for i in range(len(np.unique(y))): # one node for every unique y value (each category)
+                layers[-1].nodes.append(node(np.ones(self.h_layers[-1])))
+        else:
+            layers.append(layer(len(layers), list([node(np.ones(self.h_layers[-1]))]))) # for regression and prob estimation, we only need one output node
+
+        # fill in the child nodes for every node
+        for i in range(len(layers)-1): # cycle through all layers but the output layer
+            for neuron in layers[i].nodes: # cycle through every node in that layer
+                neuron.child_nodes = neuron.child_nodes + layers[i+1].nodes # because network is fully connected, append every node in the next layer as a child node
+
+        # fill in the parent nodes for every node
+        for i in range(1,len(layers)):
+            for neuron in layers[i].nodes:
+                neuron.parent_nodes = neuron.parent_nodes + layers[i-1].nodes
+
+        # initialize the w parameters to random float between [-5,5]
+        for i in range(1, len(self.h_layers)+1):
+            for neuron in layers[i].nodes:
+                neuron.w.extend(np.random.uniform(-5,5,len(layers[i-1].nodes)))
+
+        self.layers = layers
+        return layers
+
+    def train(self, x, y):
+        # use the global classes layer and node
+        global layer
+        global node
+
+        iteration = 0 # counter for the iterations
+        learning_rate = self.learning_rate
+
+        # concatenate x and y together to randomly sample from it later
+        mini_batch = np.concatenate((x,y),1)
+
+        while self.early_stopping(x,y,iteration) == False: # check for convergence and early stopping criteria
+            # randomly select subset of the data for mini-batch stochastic gradient descent
+            np.random.shuffle(mini_batch) # shuffle the data
+            mini_batch = mini_batch[:int(x.shape[0]*self.batch_size),:]
+
+            layers = self.layers # get the updated layers from the class attributes
+            for sample in range(mini_batch.shape[0]):
+                # predict on the current point. the predict function also calculates all node values for this training point
+                error, prediction = self.predict(np.array(mini_batch[sample,0]).reshape(1,1),np.array(mini_batch[sample,1]).reshape(1,1))
+
+                for neuron in layers[-1].nodes: # calculate partial derivatives for nodes in the last layer
+                    neuron.derivative = (-2/mini_batch.shape[0])*(mini_batch[sample,1]-neuron.value)
+
+                # calculate partial derivatives of every node based on its child node
+                for layer in range(2,len(layers)): # cycle backwards through 2nd last to second layer (we don't need derivative of the input layer)
+                    for neuron in range(len(layers[-layer].nodes)): # cycle through nodes per layer
+                        for child in range(len(layers[-layer].nodes[neuron].child_nodes)): # cycle through child nodes per node
+                            if self.activ_func == 'relu':
+                                if layers[-layer].nodes[neuron].child_nodes[child].value >= 0: # if statement because of ReLU activation function
+                                    layers[-layer].nodes[neuron].derivative = layers[-layer].nodes[neuron].child_nodes[child].w[neuron] * layers[-layer].nodes[neuron].child_nodes[child].derivative
+                                else:
+                                    layers[-layer].nodes[neuron].derivative = 0
+                            elif self.activ_func == 'softplus':
+                                layers[-layer].nodes[neuron].derivative = (layers[-layer].nodes[neuron].child_nodes[child].derivative * layers[-layer].nodes[neuron].child_nodes[child].w[neuron] * np.exp(layers[-layer].nodes[neuron].child_nodes[child].value))/(1+np.exp(layers[-layer].nodes[neuron].child_nodes[child].value))
+
+                # to get the partial derivative of a weight parameter, multiply the
+                # derivative of the current node with the value of the parent node
+                for layer in range(1,len(layers)): # cycle from last to second layer (the first doesn't have parameters)
+                    for neuron in range(len(layers[-layer].nodes)): # cycle through nodes per layer
+                        if self.activ_func == 'relu':
+                            if layers[-layer].nodes[neuron].value >= 0: # if statement because of ReLU activation function
+                                layers[-layer].nodes[neuron].b -= learning_rate * layers[-layer].nodes[neuron].derivative
+                        elif self.activ_func == 'softplus':
+                            layers[-layer].nodes[neuron].b -= (learning_rate * layers[-layer].nodes[neuron].derivative * np.exp(layers[-layer].nodes[neuron].value))/(1+np.exp(layers[-layer].nodes[neuron].value))
+                        for weight in range(len(layers[-layer].nodes[neuron].w)): # cycle through weight per node
+                            if self.activ_func == 'relu':
+                                if layers[-layer].nodes[neuron].value >= 0: # if statement because of ReLU activation function
+                                    layers[-layer].nodes[neuron].w[weight] -= learning_rate * layers[-layer].nodes[neuron].parent_nodes[weight].value * layers[-layer].nodes[neuron].derivative
+                            elif self.activ_func == 'softplus':
+                                layers[-layer].nodes[neuron].w[weight] -= (learning_rate * layers[-layer].nodes[neuron].parent_nodes[weight].value * np.exp(layers[-layer].nodes[neuron].value) * layers[-layer].nodes[neuron].derivative)/(1+np.exp(layers[-layer].nodes[neuron].value))
+
+            iteration += 1 # increment the iteration counter
+
+        errors, preds = self.predict(x,y) # predict with the adjusted parameters
+        print('Iterations until stopped:', iteration)
+        print('Training error (per sample):', errors[0]/x.shape[0])
+
+    def early_stopping(self, x, y, iteration):
+        # use the global variables
+        global last_error
+        global test_error
+        global old_layers
+        global return_counter
+        error, predictions = self.predict(x,y) # predict on the test data
+
+        # check if algorithm has converged
+        if iteration == 0: # if it's the first iteration, skip everything here right away
+            return_counter = 0 # variable counts how many times we returned back to the old model
+            test_error = [999999999999999] # insert large number, so that the error will be smaller for the first iteration
+            last_error = test_error[0] # error of last iteration to compare if it changed (convergence check)
+            return False
+        else:
+            if np.sqrt((error[0]-last_error)**2) < 0.000001: # if error barely changes with an iteration, declare convergence
+                if iteration > 1000:
+                    print('...algorithm converged!')
+                    return True
+                else:
+                    print('...converged, but not enough iterations')
+
+        last_error = error[0]
+
+        # check if test error has decreased over the last 50 iterations
+        if iteration % 50 == 0: # check only every 50'th iteration
+            test_error.append(error[0])
+            if test_error[-1] < test_error[-2]: # check if error is less than last time
+                old_layers = self.layers # safe network in case the next one performs worse
+                return False
+            else:
+                print('...returned to old parameters...')
+                return_counter += 1 # increment return-to-old-model-count
+                if return_counter > 20:
+                    return True
+                self.layers = old_layers # continue training with old network if new performs worse
+                return False
+        else:
+            return False
+
+# add classification
+# add convolutional neural networks
+
+model = ffnn([3,3], activ_func = 'softplus', learning_rate = 0.1, batch_size = 1) # call the constructor and set the parameters
+model.construct(x) # construct the network with the input data (needs it for shape of input layer)
+model.train(x,y) # train the network with training data
+model.predictions(x,y) # predict on the test data with the trained model
